409e2ab0dd5cdd327acd6cca36ea9a2928a68b71
dmitriy buzin <enterdemon@mail.ru>
2022-09-04 23:47:41 +0300

    first

---
+++ src/main/scala/example.scala
@@ -0,0 +1,20 @@
+import org.json4s.jackson
+
+object example {
+
+  def main(args: Array[String]):Unit = {
+
+    val jsonString = os.read(os.pwd/"spark_config.json")
+
+    val jsonString2 = os.read(os.pwd/"other.json")
+
+    var json = jackson.JsonMethods.parse(jsonString)
+
+    var data = ujson.read(jsonString)
+
+    print(json)
+
+
+
+  }
+}
